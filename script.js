d02f9983b155dc5fdcccab0d46195474d0c93576
Danial <menunem6@gmail.com>
2024-12-10 21:59:25 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const heading = document.getElementById("heading");
+heading.innerHTML = '<h1 style color red>Welcome to Kazakhstan<\h1>'
+
+
+const sekend = document.getElementById('#sekend')
+sekend.textContent = '<h2>City Almaty<\h2>'
+
+const sir = document.getElementById('#lll')
+sir.textContent = 'Maulenova 92'
+
+const ris = document.getElementById('#rrr')
+ris.textContent = 'Cyber school Amjilt'
+
+const myName = document.getElementById('#name')
+myName.textContent = 'Danial Shariplhan'
